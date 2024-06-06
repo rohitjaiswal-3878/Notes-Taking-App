33e3850e2a96866b4d3300bdf6cc0a44c8d8552d
rohitjaiswal-3878 <rj710910@gmail.com>
2024-06-06 08:44:21 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PocketNotes from "./pages/PocketNotes";
+
+function App() {
+  return (
+    <div>
+      <PocketNotes />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/PocketNotes/index.jsx
@@ -0,0 +1,8 @@
+import "./index.css";
+import React from "react";
+
+function PocketNotes() {
+  return <div>Hello world</div>;
+}
+
+export default PocketNotes;
