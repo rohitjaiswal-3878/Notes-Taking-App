c52861f1a31221588dc825a6fcb65b6b672b8a42
rohitjaiswal-3878 <rj710910@gmail.com>
2024-06-07 23:40:33 +0530

    Added Pocket Notes initial page.

--- src/pages/PocketNotes/index.jsx
+++ src/pages/PocketNotes/index.jsx
@@ -1,8 +1,24 @@
 import "./index.css";
 import React from "react";
+import peopleImg from "../../assets/people_img.png";
+import lockIcon from "../../assets/lock_icon.png";
 
 function PocketNotes() {
-  return <div>Hello world</div>;
+  return (
+    <div className="container-no-notes">
+      <img src={peopleImg} alt="people image" />
+      <span className="heading">Pocket Notes</span>
+      <p className="content">
+        Send and receive messages without keeping your phone online.
+        <br />
+        Use Pocket Notes on up to 4 linked devices and 1 mobile phone
+      </p>
+      <div className="encrypted">
+        <img src={lockIcon} alt="lock icon" />
+        <span>end-to-end encrypted</span>
+      </div>
+    </div>
+  );
 }
 
 export default PocketNotes;
